de3d0e96b9ba94d93e684d5a3f5512a8d6c5aaf5
MarcosCruzOn <marcoscruzon@gmail.com>
2026-02-24 12:03:19 -0300

    feat: api paths

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,44 @@ app.get('/', (req, res) => {
 	res.json({ message: 'Hello, World!' })
 })
 
+app.post('/auth/login', (req, res) => {
+	const { email, password } = req.body
+	console.log(`Login attempt with email: ${email} and password: ${password}`)
+	res.send()
+})
+
+app.post('/partners', (req, res) => {
+	const { name, email, password, company_name } = req.body
+})
+
+app.post('/costumers', (req, res) => {
+	const { name, email, password, telephone, address } = req.body
+})
+
+app.post('/events', (req, res) => {
+	const { name, description, date, location } = req.body
+})
+
+app.post('/partners/events', (req, res) => {
+	const { name, description, date, location } = req.body
+})
+
+app.get('/events', (req, res) => {})
+
+app.get('/partners/events', (req, res) => {})
+
+app.get('/events/:eventId', (req, res) => {
+	const { eventId } = req.params
+	res.send(`Event ID: ${eventId}`)
+	console.log(`Fetching details for event ID: ${eventId}`)
+})
+
+app.get('/partners/events/:eventId', (req, res) => {
+	const { eventId } = req.params
+	res.send(`Event ID: ${eventId}`)
+	console.log(`Fetching details for event ID: ${eventId}`)
+})
+
 app.listen(3000, () => {
 	console.log('Server is running on port 3000')
 })
